c29a179c12640b49557f93c54fced9bccc27648a
Davy De Waele <ddewaele@gmail.com>
2017-06-05 16:07:14 +0200

    commit to test update procedure

--- src/main/java/com/example/CrudSampleApplication.java
+++ src/main/java/com/example/CrudSampleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Spring Boot Application class
+ * Spring Boot Application, used for demo purposes
  */
 @SpringBootApplication
 public class CrudSampleApplication {
